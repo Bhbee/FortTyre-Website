38afd245ed8f6378d5e34d3d615443b7ede1415d
David Olaleye <39854146+Pisces2802@users.noreply.github.com>
2023-05-19 17:10:02 -0700

    Delete src directory

--- src/Components/ContentContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-type ContainerProps = {
-  children: ReactNode;
-}
-
-export default function ContentContainer({ children }: ContainerProps) {
-  return <section>{children}</section>;
-}
