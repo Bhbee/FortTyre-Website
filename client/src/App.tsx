db4f3de6669989cc0758602c0763aecb05c85479
David Olaleye <olaoluwaolaleye@gmail.com>
2023-05-19 17:03:27 -0700

    Add client folder

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import ContentContainer from './Components/ContentContainer';
+import Layout from './Components/Layout';
+import './App.css';
+
+function App() {
+  return (
+  <div className="text-base">
+     <ContentContainer>
+     <Layout />
+     </ContentContainer>
+  </div>
+   
+  );
+}
+
+export default App;
---
+++ client/src/Components/Layout.tsx
@@ -0,0 +1,17 @@
+
+function Layout() {
+  return (
+    <nav>
+      <ul>
+        <li>Logo</li>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contatct</li>
+        <li>Terms</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Layout;
