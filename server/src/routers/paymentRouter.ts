8054240cf8aa88229c8c6b891530a8b200afe443
Adetayo Boluwatife Chritiana <Samuelchristiana38@gmail.com>
2023-07-07 17:41:30 +0100

    Deleted payment utility as it's no longer needed

--- server/src/routers/paymentRouter.ts
+++
@@ -1,6 +0,0 @@
-import express, {Request, Response} from 'express'
-import { PayWithPaystack, VerifyPayment} from '../controllers/payment.controller'
-export const paymentRouter = express.Router()
-
-paymentRouter.post('/pay', PayWithPaystack)
-paymentRouter.get('/verify', VerifyPayment)
